652c484655ef93999ac3fa6fbf8dbc552971495f
SayyedTaheraEhsan <s.tahera123@gmail.com>
2024-08-27 15:08:44 +0530

    this program is prim no

---
+++ prim.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int n,div,ans;
+    printf("enter the no\n");
+    scanf("%d",&n);
+    int flag=1;
+    for(int i=2;i<n-1;i++)
+    {
+     if(n%i==0)
+     flag=0;
+    }
+    if(flag)
+    printf("uor no %d is prim\n",n);
+    else
+    printf("uour no is not prim\n");
+    return 0;
+
+}
