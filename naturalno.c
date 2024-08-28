a3d4e7b0b705d5b826388c0e5932efe237d7173f
SayyedTaheraEhsan <s.tahera123@gmail.com>
2024-08-28 16:16:08 +0530

    this program natralno

---
+++ naturalno.c
@@ -0,0 +1,13 @@
+//**Write a `while` loop that prints the first 5 natural numbers.**
+#include<stdio.h>
+int main ()
+{
+    int i=1;
+    
+    while(i<=5)
+    {
+    printf("%d\n",i);
+    i++;
+    }
+ return 0;
+}
