cbcc6c7795632e06f8907041dbee6f9525d5483f
SayyedTaheraEhsan <s.tahera123@gmail.com>
2024-09-05 17:45:42 +0530

    this program is paper 1 q2  2

--- p1q22.c
+++ p1q22.c
@@ -2,12 +2,20 @@
 
 int main()
 {
-    long double x,result;
-    printf("Enter value of x: ");
-    scanf("%Lf",&x);
-
-    result = (x*x*x) + (5*x*x) + (3*x);
-    result = result / x;
-    printf("f(%.4Lf) = %.4Lf\n",x,result);
+    int x;
+    printf("Enter integer value score out of 100: ");
+    scanf("%d",&x);
+    if(x>=90)
+        printf("A\n");
+    else if(x>=80 && x<=89)
+        printf("B\n");
+    else if(x>=70 && x<=79)
+        printf("C\n");
+    else if(x>=60 && x<=69)
+        printf("D\n");
+    else
+        printf("Invalid score or fail!\n");
     return 0;
+    
 }
+	
