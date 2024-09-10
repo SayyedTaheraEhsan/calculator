a8090c422e833f372d4cf185310ff25a67db75b2
SayyedTaheraEhsan <s.tahera123@gmail.com>
2024-09-10 10:30:14 +0530

    this program is size bases

--- p2q33.c
+++ p2q33.c
@@ -2,32 +2,30 @@
 
 int main()
 {
-    int m,n,i,j,sum=0,rich,max;
-    printf("Enter number of Rows: ");
-    scanf("%d",&m);
-    printf("Enter number of Columns: ");
-    scanf("%d",&n);
-    int M1[m][n];
-    for(i=0;i<m;i++){
-        printf("Row %d : \n",i+1);
-        for(j=0;j<n;j++){
-            scanf("%d",&M1[i][j]);
+
+    int n = 3,m = 3;
+    int mat[n][m] = {{0,0,0},{0,0,1},{0,1,1}};
+    int row,count,max_row,max_count = 0;
+    for (int i = 0; i < n; ++i)
+    {
+        row = i;
+        count = 0;
+        for (int j = 0; j < m; ++j)
+        {
+            if (mat[i][j] == 1)
+            {
+                count++;
+            }
+            
+        }
+
+        if (count > max_count)
+        {
+            max_row = row;
+            max_count = count;
         }
     }
+
+    printf("[%d,%d]\n",max_row,max_count);
     
-    max=sum;
-    for(i=0;i<m;i++){
-        sum=0;
-        for(j=0;j<n;j++){
-            sum+=M1[i][j];
-        }
-        if(max<sum){
-            max=sum;
-            rich=i;
-        }
-    }
-    
-    printf("Output: [%d,%d]\n",rich,max);
-    
-    return 0;
 }
