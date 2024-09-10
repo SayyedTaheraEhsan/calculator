565d04075210430bb84c1514917f0c4b071bf73e
SayyedTaheraEhsan <s.tahera123@gmail.com>
2024-09-10 10:31:31 +0530

    this program is size bases

--- p2q32.c
+++ p2q32.c
@@ -1,52 +1,63 @@
-#include<stdio.h>
-
+#include <stdio.h>
+#include <stdbool.h>
+bool plot_flowers(int flowerbed[],int n, int new_f)
+{
+    
+    for (int i = 0; i < n; ++i)
+    {
+        bool left = false;
+        bool right = false;
+        if (flowerbed[i] == 0)
+        {
+            if (i == 0 || flowerbed[i - 1] == 0){
+                left = true;
+            }
+            if (i == n - 1 || flowerbed[i + 1] == 0){
+                right = true;
+            }
+            if (left && right)
+            {
+                flowerbed[i] = 1;
+                new_f--;
+            }
+        }
+    }
+    return new_f;
+}
 int main()
 {
-    int n,i,j,k,temp;
+    int n, new_f;
+    printf("Enter the size of flowerbed: ");
+    scanf("%d", &n);
 
-    printf("Enter number of rows and columns for Square Matrix : ");
-    scanf("%d",&n);
+    int flowerbed[n];
 
-    int M[n][n];
-    printf("Enter elements for Matrix : ");
-    for(i=0;i<n;i++){
-        for(j=0;j<n;j++){
-            printf("M[%d][%d] : ",i+1,j+1);
-            scanf("%d",&M[i][j]);
-        }
-    }
-    printf("Original Matrix : \n");
-    for(i=0;i<n;i++){
-        printf("[");
-        for(j=0;j<n;j++){
-            printf(" %d ",M[i][j]);
-        }
-        printf("]\n");
+    for (int i = 0; i < n; ++i)
+    {
+        printf("Flower : 1 \t No Flower : 0 ");
+        scanf("%d", &flowerbed[i]);
     }
 
-    printf("Horizontally Flipped and Inverted Matrix : \n");
+    printf("Enter number of new flowers to be plotted: ");
+    scanf("%d", &new_f);
 
-    for(i=0;i<n;i++){
-        for(j=n-1,k=0;j>=0,k<n;j--,k++){
-            
-            temp=M[i][j];
-            M[i][j]=M[i][k];
-            M[i][k] = temp;
-            if(M[i][j]){
-                M[i][j]=0;
-            }
-            else{
-                M[i][j]=1;
-            }
-        }
+    printf("\n");
+    for (int i = 0; i < n; ++i)
+    {
+        printf("%d\t", flowerbed[i]);
     }
-    for(i=0;i<n;i++){
-        printf("[");
-        for(j=0;j<n;j++){
-            printf(" %d ",M[i][j]);
-        }
-        printf("]\n");
+     printf("\n");
+
+    bool plotted = plot_flowers(flowerbed, n, new_f);
+    if (plotted){
+        printf("%d\n",plotted);
+    }
+    else{
+         printf("%d\n",plotted);
     }
 
-    return 0;
+    for (int i = 0; i < n; ++i)
+    {
+        printf("%d\t", flowerbed[i]);
+    }
 }
